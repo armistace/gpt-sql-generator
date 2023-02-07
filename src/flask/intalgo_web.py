8722e4ba7d8c78de885443db963167a83642e3e9
Andrew Ridgway <ar17787@gmail.com>
2023-02-07 17:51:33 +1000

    rendering dbt logs

--- src/flask/intalgo_web.py
+++ src/flask/intalgo_web.py
@@ -1,6 +1,14 @@
 import modeller.dbt_docker as dbt
+import logging
 
-from flask import Flask
+from flask import Flask, render_template
+from markupsafe import Markup, escape
+import re
+
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 
 
 app = Flask(__name__)
@@ -8,8 +16,12 @@ app = Flask(__name__)
 @app.route('/')
 def run_dbt_test():
     dbt_test = dbt.docker_dbt()
-    output = dbt_test.run_dbt_test()
-    return output
+    cont_logs = dbt_test.run_dbt_test()
+    #docker returns a byte array this maps it to make it readable
+    output = Markup("<br>".join(re.split(r'(?:\r\n|\r|\n)', escape(''.join(map(chr, cont_logs))))))
+
+    log.info(output)
+    return render_template('logs.html', logs=output)
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=80)
