bbb421030a9ca85fe3c4c636fe5a54ab57a6a147
Andrew Ridgway <ar17787@gmail.com>
2023-03-16 17:55:48 +1000

    openai creating sql

--- src/dbt/open_ai.py
+++ src/dbt/open_ai.py
@@ -3,8 +3,17 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+with open ('intalgo/models/sources.yml', 'r') as source_file:
+    sources = source_file.read() 
+
+sources_prompt = f"Using the input dbt sources yaml of {sources} Generate a DBT compliant sql file that answers the question "
+
 query = input("ENTER CHATGPT QUERY: ")
 
-response = openai.Completion.create(model="text-davinci-003", prompt=query )
+#print(f"{sources_prompt}{query}")
+
+response = openai.Completion.create(model="text-davinci-003", prompt=f"{sources_prompt}{query}", max_tokens=100)
 print("############RESPONSE##################")
-print(response.choices[0].text)
+
+for line in response.choices:
+    print(line.text)
